af992ab012940c010cacbf6d26fddffb4cfc36eb
Albina Zhumabaeva <96684628+albnzhum@users.noreply.github.com>
2022-09-26 13:37:51 +0500

    Add files via upload

---
+++ Pz_6/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Pz_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число n: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int f1 = 1;
+            int f2 = 1;
+            int sum = 0;
+            while (n>=sum)
+            {
+                sum = f1 + f2;
+                
+                f1 = f2;
+                f2 = sum;
+                
+                
+            }
+            Console.Write("N-oe число Фибоначчи: ");
+            Console.WriteLine("{0} ", sum);
+
+        }
+    }
+}
