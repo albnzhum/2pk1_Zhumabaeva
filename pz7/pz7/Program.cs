1d9c6cd230261b64e35effbfbb81f57b5b3fa0a2
Albina Zhumabaeva <96684628+albnzhum@users.noreply.github.com>
2022-10-03 23:05:45 +0500

    Add files via upload

---
+++ pz7/pz7/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Pz_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[10];//объявление массива
+            Random rand = new Random();
+            for (int i = 0; i < 10; i++)
+            {
+                array[i] = rand.Next(0, 4); //заполнение массива рандомными числами в диапазоне от 0 до 4
+                Console.Write(array[i] + " ");
+
+            }
+
+            for (int i = 0; i > 10; i++)
+            {
+                if (array[i] == array[i + 1])
+                {
+                    Console.WriteLine("есть"); //помогите пожалуйста у меня здесь ошибка а я не понимаю почему
+                }
+
+            }
+
+        }
+    }
+}
