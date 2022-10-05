e00655f9e60b0390bbe11d02ef4dd98c155767e9
Albina Zhumabaeva <96684628+albnzhum@users.noreply.github.com>
2022-10-05 23:06:26 +0500

    Add files via upload

---
+++ pz8/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace pz8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] a = new int[4, 5];
+            Random r = new Random();
+            for (int i = 0; i <4; i++)
+            {
+                for (int j =0; j<5; j++)
+                {
+                    a[i,j] = r.Next(10);
+                    Console.Write(a[i,j] + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("Максимальные элементы в каждом столбце");
+            for (int i = 0; i < a.GetLength(1); i++)
+            {
+                int tempmax = int.MinValue;
+                for (int j = 0; j < a.GetLength(0); j++)
+                {
+                    if (a[j,i] > tempmax)
+                    {
+                        tempmax = a[j, i];
+                    }
+
+                }
+                Console.Write(tempmax + " ");
+            }
+        }
+    }
+}
