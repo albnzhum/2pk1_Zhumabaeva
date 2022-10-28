1d85e314984eaa4ef7dc0f2934ab8dddb9c237e9
Albina Zhumabaeva <96684628+albnzhum@users.noreply.github.com>
2022-10-28 19:28:11 +0500

    Add files via upload

---
+++ pz_10/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace pz_10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите строку:");
+            string s = Console.ReadLine(); //ввод строки
+            Console.WriteLine("Введите количество звездочек: ");
+            int n = Convert.ToInt32(Console.ReadLine()); // количество звездочек 
+            for (int i = 0; i < s.Length; i++)
+            {
+                Console.Write(s[i] + new string('*', n));
+            }
+        }
+    }
+}
