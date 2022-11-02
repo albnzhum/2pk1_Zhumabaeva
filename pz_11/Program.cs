f4720dd951503407d57e86d7c1af0d3a4d9bc70a
Albina Zhumabaeva <96684628+albnzhum@users.noreply.github.com>
2022-11-02 17:46:06 +0500

    Add files via upload

---
+++ pz_11/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace pz_11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите ссылку в формате https//example.com");
+            string link = Console.ReadLine();
+
+            if (link.Contains("https//")) //проверка на правильный ввод ссылки
+            {
+                string a = link.Substring(link.IndexOf('.')+1); //извлекаем подстроку до начала домена
+                string domen = a.Remove(a.IndexOf('/')); //извлекаем из 1-й подстроки все остальное
+                Console.WriteLine("Ваш домен - " + domen);
+
+            }
+            else
+            {
+                Console.WriteLine("Введите правильную ссылку");
+                Console.ReadLine();
+
+            }
+            
+        }
+    }
+}
