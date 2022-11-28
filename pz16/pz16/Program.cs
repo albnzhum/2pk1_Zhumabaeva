429471f45704c06ca1c339187a7a428f6d3f6eb1
Albina Zhumabaeva <96684628+albnzhum@users.noreply.github.com>
2022-11-28 10:01:58 +0500

    16 практика

---
+++ pz16/pz16/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+
+namespace pz16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string emptyString = ""; // пустая строка
+            char[] letters; //массив символов
+            StreamReader file = new StreamReader("test.txt"); //чтение файла
+
+            while (file.EndOfStream != true)
+            {
+                emptyString += file.ReadLine(); //присваиваем весь текст из файла
+            }
+            letters = emptyString.ToCharArray(); //в массив символов
+            Console.WriteLine($"Количество символов: {letters.Length}");
+            file.Close();
+        }
+    }
+}
