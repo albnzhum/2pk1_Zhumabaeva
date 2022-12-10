f2642eb2d612578db1d536504f3c99f92017c7df
Albina Zhumabaeva <96684628+albnzhum@users.noreply.github.com>
2022-12-10 09:23:33 +0500

    на плюсах

---
+++ pz17/pz17.cpp
@@ -0,0 +1,25 @@
+﻿
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    string text = "";
+    cout << "Введите текст:" << endl;
+    getline(cin, text);
+    int space = 0;
+    char* chars = new char[text.length() + 1];
+    text.copy(chars, text.length());
+    chars[text.length()] = '\0';
+    for (int i = 0; i <= text.length() +1; i++)
+    {
+        if (chars[i] == ' ') 
+        {
+            space++;
+        }
+    }
+    cout << "Количество пробелов в строке: " << space;
+
+}
