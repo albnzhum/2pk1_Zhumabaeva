995bc69f004c893095c899baaecc7d13c70e27fc
Albina Zhumabaeva <96684628+albnzhum@users.noreply.github.com>
2022-12-10 08:43:26 +0500

    Add files via upload

---
+++ pz15/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Reflection.PortableExecutable;
+using System.Runtime.Intrinsics.Arm;
+using System.Threading;
+
+namespace pz15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string emptyString = ""; // пустая строка
+            char[] letters; //массив символов
+            int linesCount = 0;
+            StreamReader file = new StreamReader("test.txt"); //чтение файла
+
+            while (file.EndOfStream != true)
+            {
+                emptyString += file.ReadToEnd(); //присваиваем весь текст из файла
+            }
+            letters = emptyString.ToCharArray(); //в массив символов
+            Console.WriteLine(emptyString);
+            for (int i = 0; i < letters.Length; i++)
+            {
+                if (letters[i] == '\n')
+                {
+                    linesCount++; 
+                }
+            }
+            Console.WriteLine($"Количество строк: {linesCount}");
+            file.Close();
+        }
+    }
+}
