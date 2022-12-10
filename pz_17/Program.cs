c79c638e8c80fd8e7edec7daacc3da98499faf78
Albina Zhumabaeva <96684628+albnzhum@users.noreply.github.com>
2022-12-10 08:45:49 +0500

    на шарпе

---
+++ pz_17/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace pz_17
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            unsafe {
+                string text = Console.ReadLine();
+                int space = 0;
+                fixed (char *length = text) {
+                    for (int i = 0; length[i] != 0; i++)
+                        if(length[i] == ' ')
+                        {
+                            space++;
+                        }
+                        
+                }
+                Console.WriteLine($"Количество пробелов в строке: {space}");
+                
+
+            }
+            
+        }
+    }
+}
