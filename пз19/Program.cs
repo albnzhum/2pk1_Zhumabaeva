e8933416478b065007ea91380ff5a394fa7848fd
Albina Zhumabaeva <96684628+albnzhum@users.noreply.github.com>
2022-12-16 23:06:08 +0500

    19 пзшка 1 текст

---
+++ пз19/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace пз19
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FileStream file = new FileStream("text.txt", FileMode.Open, FileAccess.Read);
+            StreamReader reader = new StreamReader(file);
+            //Console.WriteLine(reader.ReadToEnd());
+            
+
+            string text = reader.ReadToEnd();
+            file.Close();
+
+            //Console.WriteLine(text);
+            string Pattern = @"([А-Я][а-я]+\s?[А-Я][а-я]+)\s[+7]";
+
+            Regex regex = new Regex(Pattern);
+            foreach (Match match in regex.Matches(text))
+            {
+                Console.WriteLine(match.Value);
+            }
+            //Console.WriteLine(names);
+
+        }
+    }
+}
